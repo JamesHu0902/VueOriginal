08ddd4f2ffd78b0e4db787627386f9071929ae63
JamesHu <james780902@gmail.com>
2019-08-05 15:16:32 +0800

    基礎套件建置完畢

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+// 引用 BS4
+import "bootstrap";
 
-Vue.config.productionTip = false
+// 引用 AJAX 套件
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+// 引用 Loading 套件
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+Vue.component("Loading", Loading);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
